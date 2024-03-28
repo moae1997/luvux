8842754d4d2556aea01ad7f22344d79d5a0f591d
Amna Abdallah <amnaabdallah@Amnas-Air.lan>
2024-03-27 20:36:14 -0400

    update

--- index.js
+++ index.js
@@ -52,43 +52,49 @@ app.get('/api/customers', async(req, res, next)=> {
 
 
 
-const init = async()=> {
-  await client.connect();
-  console.log('connected to database');
-  await createTables();
-  console.log('tables created');
-  const [jo, bow, john, bill, cathy, blackSeedDounut, bodyScrub, sandlewoodCandle, honey] = await Promise.all([
-    createCustomer({ username: 'jo', password: 'uxirjgb' }),
-    createCustomer({ username: 'cathy', password: 'sieugbi!!' }),
-    createCustomer({ username: 'bow', password: 'sfkdjhb' }),
-    createProduct({ name: 'blackSeedDounut', price: 5}),
-    createProduct({ name: 'bodyScrub', price: 5}),
-    createProduct({ name: 'sandlewoodCandle', price: 7}),
-    createProduct({ name: 'honey', price: 10}),
-  ]);
-  console.log(jo.id);
+  const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    console.log('tables created');
+  
+    const [jo, cathy, bow, blackSeedDounut, bodyScrub, sandlewoodCandle, honey] = await Promise.all([
+      createCustomer({ username: 'jo', password: 'uxirjgb' }),
+      createCustomer({ username: 'cathy', password: 'sieugbi!!' }),
+      createCustomer({ username: 'bow', password: 'sfkdjhb' }),
+      createProduct({ name: 'blackSeedDounut', price: 5}),
+      createProduct({ name: 'bodyScrub', price: 5}),
+      createProduct({ name: 'sandlewoodCandle', price: 7}),
+      createProduct({ name: 'honey', price: 10}),
+    ]);
+    console.log("jo:", jo);
+    
 
-
-  const customers = await fetchCustomers();
-  console.log(customers);
-  const products = await fetchProducts();
-  console.log(products);
-
-  const carts = await Promise.all([
-    createCart({ customer_id: jo.id, product_id: honey.id}),
-    createCart({ customer_id: cathy.id, product_id: blackSeedDounut.id}),
-    createCart({ customer_id: bow.id, product_id: sandlewoodCandle.id}),
-    createCart({ customer_id: john.id, product_id: bodyScrub.id}),
-  ]);
-
-  console.log(await fetchCart(jo.id));
-
-  await deleteCart(carts[0].id);
-  console.log(await fetchCart(jo.id));
-
-  const port = process.env.PORT || 3000;
-  app.listen(port, ()=> console.log(`listening on port ${port}`));
-
-};
-
-init();
+    console.log(jo.id);
+  
+    const customers = await fetchCustomers();
+    console.log(customers);
+    const products = await fetchProducts();
+    console.log(products);
+    console.log("bodyScrub:", sandlewoodCandle);
+  
+    const customerCarts = await Promise.all([
+      createCart({ customer_id: jo.id, product_id: honey.id}),
+      createCart({ customer_id: cathy.id, product_id: blackSeedDounut.id}),
+      createCart({ customer_id: bow.id, product_id: sandlewoodCandle.id}),
+      createCart({ customer_id: jo.id, product_id: bodyScrub.id}),
+    ]);
+  
+    console.log(customerCarts);
+  
+    console.log(await fetchCart(jo.id));
+  
+    await deleteCart(customerCarts[1].id);
+    console.log(await fetchCart(cathy.id));
+  
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  
+  };
+  
+  init();
